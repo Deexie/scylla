scylladb/scylladb

locator: service: add rebuild_v2 tablet transition kind
Currently, in the streaming stage of rebuild tablet transition,
we stream tablet data from all replicas.
This patch series splits the streaming stage into two phases:
- streaming phase, where we stream tablet data from one replica;
- repair phase, where we repair the tablet.

To differentiate the two streaming methods, a new tablet transition
kind - rebuild_v2 - is added.

The transtions and stages for rebuild_v2 transition kind will be
added in the following patches.

locator/tablets.cc
@@ -140,6 +140,25 @@ tablet_migration_streaming_info get_migration_streaming_info(const locator::topo
             });
 
             return result;
+        case tablet_transition_kind::rebuild_v2: {
+            if (!trinfo.pending_replica.has_value()) {
+                return result; // No nodes to stream to -> no nodes to stream from
+            }
+
+            auto s = std::unordered_set<tablet_replica>(trinfo.next.begin(), trinfo.next.end());
+            erase_if(s, [&] (const tablet_replica& r) {
+                if (r == *trinfo.pending_replica) {
+                    return false;
+                }
+                auto* n = topo.find_node(r.host);
+                return !n || n->is_excluded();
+            });
+            result.stream_weight = locator::tablet_migration_stream_weight_repair;
+            result.read_from = s;
+            result.written_to = std::move(s);
+
+            return result;
+        }
         case tablet_transition_kind::repair:
             auto s = std::unordered_set<tablet_replica>(tinfo.replicas.begin(), tinfo.replicas.end());
             result.stream_weight = locator::tablet_migration_stream_weight_repair;
@@ -544,6 +563,7 @@ static const std::unordered_map<tablet_transition_kind, sstring> tablet_transiti
         {tablet_transition_kind::migration, "migration"},
         {tablet_transition_kind::intranode_migration, "intranode_migration"},
         {tablet_transition_kind::rebuild, "rebuild"},
+        {tablet_transition_kind::rebuild_v2, "rebuild_v2"},
         {tablet_transition_kind::repair, "repair"},
 };
 

Deexie/scylla

- repair phase, where we repair the tablet;
- streaming phase, where we stream tablet data from one replica.
(cherry picked from commit ed7b8bb787eec9498995133ba37822e17b1450ad)

locator/tablets.cc
@@ -141,6 +141,23 @@ tablet_migration_streaming_info get_migration_streaming_info(const locator::topo
             });
 
             return result;
+        case tablet_transition_kind::rebuild_v2: {
+            if (!trinfo.pending_replica.has_value()) {
+                return result; // No nodes to stream to -> no nodes to stream from
+            }
+
+            auto s = std::unordered_set<tablet_replica>(tinfo.replicas.begin(), tinfo.replicas.end());
+            erase_if(s, [&] (const tablet_replica& r) {
+                auto* n = topo.find_node(r.host);
+                return !n || n->is_excluded();
+            });
+            result.stream_weight = locator::tablet_migration_stream_weight_repair;
+            result.read_from = s;
+            result.written_to = std::move(s);
+            result.written_to.insert(*trinfo.pending_replica);
+
+            return result;
+        }
         case tablet_transition_kind::repair:
             auto s = std::unordered_set<tablet_replica>(tinfo.replicas.begin(), tinfo.replicas.end());
             result.stream_weight = locator::tablet_migration_stream_weight_repair;
@@ -540,6 +557,7 @@ static const std::unordered_map<tablet_transition_kind, sstring> tablet_transiti
         {tablet_transition_kind::migration, "migration"},
         {tablet_transition_kind::intranode_migration, "intranode_migration"},
         {tablet_transition_kind::rebuild, "rebuild"},
+        {tablet_transition_kind::rebuild_v2, "rebuild_v2"},
         {tablet_transition_kind::repair, "repair"},
 };
 