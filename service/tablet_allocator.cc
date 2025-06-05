scylladb/scylladb

tablets: allocator: create co-located tables in a single operation
Co-located base and child tables may be created together in a single
operation.  The tablet allocator in this case needs to handle them
together and not each table independently, because we need to have the
base schema and tablet map when creating the child tablet map.

We do this by registering the tablet allocator to the migration
notification on_before_create_column_families that announces multiple
new tables, and there we allocate tablets for all the new base tables,
and for the new child tables we create their maps from the base tables,
which are either a new table or an existing one.

service/tablet_allocator.cc
@@ -3181,6 +3181,79 @@ public:
         _use_tablet_aware_balancing = use_tablet_aware_balancing;
     }
 
+    tablet_map allocate_tablets_for_new_table(const tablet_aware_replication_strategy* tablet_rs, const schema& s) {
+        auto tm = _db.get_shared_token_metadata().get();
+        lblogger.debug("Creating tablets for {}.{} id={}", s.ks_name(), s.cf_name(), s.id());
+        auto lb = make_load_balancer(tm, nullptr, {});
+        auto plan = lb.make_sizing_plan(s.shared_from_this(), tablet_rs).get();
+        auto& table_plan = plan.tables[s.id()];
+        if (table_plan.target_tablet_count_aligned != table_plan.target_tablet_count) {
+            lblogger.info("Rounding up tablet count from {} to {} for table {}.{}", table_plan.target_tablet_count,
+                    table_plan.target_tablet_count_aligned, s.ks_name(), s.cf_name());
+        }
+        auto tablet_count = table_plan.target_tablet_count_aligned;
+        auto map = tablet_rs->allocate_tablets_for_new_table(s.shared_from_this(), tm, tablet_count).get();
+        return map;
+    }
+
+    tablet_map allocate_tablets_for_new_table_from_base(table_id base_table_id, const tablet_map& base_map) {
+        // TODO MICHAEL is it ok to copy the entire map including transitions and other fields?
+        // what if base is during transition?
+        tablet_map map(base_map);
+        map.set_base_table(base_table_id);
+        return map;
+    }
+
+    void on_before_create_column_families(const keyspace_metadata& ksm, const std::vector<schema_ptr>& cfms, std::vector<mutation>& muts, api::timestamp_type ts) override {
+        locator::replication_strategy_params params(ksm.strategy_options(), ksm.initial_tablets());
+        auto rs = abstract_replication_strategy::create_replication_strategy(ksm.strategy_name(), params);
+        if (auto&& tablet_rs = rs->maybe_as_tablet_aware()) {
+            auto tm = _db.get_shared_token_metadata().get();
+
+            std::unordered_map<table_id, schema_ptr> new_cfms_map;
+            for (auto s : cfms) {
+                new_cfms_map[s->id()] = s;
+            }
+
+            const bool colocated_tablets = _db.features().colocated_tablets;
+            std::unordered_map<table_id, std::vector<schema_ptr>> table_groups;
+            for (auto s : cfms) {
+                std::optional<table_id> base_id;
+                if (colocated_tablets) {
+                    base_id = _db.get_base_table_for_tablet_colocation(*s, new_cfms_map);
+                }
+                table_groups[base_id.value_or(s->id())].push_back(s);
+            }
+
+            for (const auto& [base_id, schemas] : table_groups) {
+                if (auto it = new_cfms_map.find(base_id); it != new_cfms_map.end()) {
+                    // base is a new table
+                    const auto& base_schema = *it->second;
+                    auto base_map = allocate_tablets_for_new_table(tablet_rs, base_schema);
+                    muts.emplace_back(tablet_map_to_mutation(base_map, base_schema.id(), base_schema.ks_name(), base_schema.cf_name(), ts, _db.features()).get());
+
+                    auto child_map = allocate_tablets_for_new_table_from_base(base_id, base_map);
+                    for (auto s_ : schemas) {
+                        const auto& s = *s_;
+                        if (s.id() != base_schema.id()) {
+                            lblogger.debug("Creating tablets for {}.{} id={} with base={}", s.ks_name(), s.cf_name(), s.id(), base_id);
+                            muts.emplace_back(tablet_map_to_mutation(child_map, s.id(), s.ks_name(), s.cf_name(), ts, _db.features()).get());
+                        }
+                    }
+                } else {
+                    // base is an existing table
+                    const auto& base_map = tm->tablets().get_tablet_map(base_id);
+                    auto child_map = allocate_tablets_for_new_table_from_base(base_id, base_map);
+                    for (auto s_ : schemas) {
+                        const auto& s = *s_;
+                        lblogger.debug("Creating tablets for {}.{} id={} with base={}", s.ks_name(), s.cf_name(), s.id(), base_id);
+                        muts.emplace_back(tablet_map_to_mutation(child_map, s.id(), s.ks_name(), s.cf_name(), ts, _db.features()).get());
+                    }
+                }
+            }
+        }
+    }
+
     void on_before_create_column_family(const keyspace_metadata& ksm, const schema& s, std::vector<mutation>& muts, api::timestamp_type ts) override {
         locator::replication_strategy_params params(ksm.strategy_options(), ksm.initial_tablets());
         auto rs = abstract_replication_strategy::create_replication_strategy(ksm.strategy_name(), params);
@@ -3192,24 +3265,9 @@ public:
                     auto base_id = *base_table;
                     auto base_map = tm->tablets().get_tablet_map(base_id);
                     lblogger.debug("Creating tablets for {}.{} id={} with base={}", s.ks_name(), s.cf_name(), s.id(), base_id);
-
-                    // TODO MICHAEL is it ok to copy the entire map including transitions and other fields?
-                    // what if base is during transition?
-                    tablet_map map(base_map);
-                    map.set_base_table(base_id);
-                    return map;
+                    return allocate_tablets_for_new_table_from_base(base_id, base_map);
                 } else {
-                    lblogger.debug("Creating tablets for {}.{} id={}", s.ks_name(), s.cf_name(), s.id());
-                    auto lb = make_load_balancer(tm, nullptr, {});
-                    auto plan = lb.make_sizing_plan(s.shared_from_this(), tablet_rs).get();
-                    auto& table_plan = plan.tables[s.id()];
-                    if (table_plan.target_tablet_count_aligned != table_plan.target_tablet_count) {
-                        lblogger.info("Rounding up tablet count from {} to {} for table {}.{}", table_plan.target_tablet_count,
-                                table_plan.target_tablet_count_aligned, s.ks_name(), s.cf_name());
-                    }
-                    auto tablet_count = table_plan.target_tablet_count_aligned;
-                    auto map = tablet_rs->allocate_tablets_for_new_table(s.shared_from_this(), tm, tablet_count).get();
-                    return map;
+                    return allocate_tablets_for_new_table(tablet_rs, s);
                 }
             });
 

Deexie/scylla

service/tablet_allocator.cc
@@ -3208,22 +3208,58 @@ public:
         return map;
     }
 
-    void on_before_create_column_family(const keyspace_metadata& ksm, const schema& s, std::vector<mutation>& muts, api::timestamp_type ts) override {
+    // Allocate tablets for multiple new tables, which may be co-located with each other, or co-located with an existing base table.
+    void allocate_tablets_for_new_tables(const keyspace_metadata& ksm, const std::vector<schema_ptr>& cfms, std::vector<mutation>& muts, api::timestamp_type ts) {
         locator::replication_strategy_params params(ksm.strategy_options(), ksm.initial_tablets());
         auto rs = abstract_replication_strategy::create_replication_strategy(ksm.strategy_name(), params);
         if (auto&& tablet_rs = rs->maybe_as_tablet_aware()) {
             auto tm = _db.get_shared_token_metadata().get();
 
-            if (auto base_table_opt = _db.get_base_table_for_tablet_colocation(s); base_table_opt && _db.features().colocated_tablets) {
-                auto base_table = *base_table_opt;
-                lblogger.debug("Creating tablets for {}.{} id={} with base={}", s.ks_name(), s.cf_name(), s.id(), base_table);
-                muts.emplace_back(colocated_tablet_map_to_mutation(s.id(), s.ks_name(), s.cf_name(), base_table, ts));
-            } else {
-                lblogger.debug("Creating tablets for {}.{} id={}", s.ks_name(), s.cf_name(), s.id());
-                tablet_map map = allocate_tablets_for_new_base_table(tablet_rs, s);
-                muts.emplace_back(tablet_map_to_mutation(std::move(map), s.id(), s.ks_name(), s.cf_name(), ts, _db.features()).get());
+            std::unordered_map<table_id, schema_ptr> new_cfms_map;
+            for (auto s : cfms) {
+                new_cfms_map[s->id()] = s;
+            }
+
+            // Group the new tables by co-location groups.
+            // The key is the base table id, which may be a new table or an existing table.
+            const bool colocated_tablets_enabled = _db.features().colocated_tablets;
+            std::unordered_map<table_id, std::vector<schema_ptr>> table_groups;
+            for (auto s : cfms) {
+                std::optional<table_id> base_id;
+                if (colocated_tablets_enabled) {
+                    base_id = _db.get_base_table_for_tablet_colocation(*s, new_cfms_map);
+                }
+                table_groups[base_id.value_or(s->id())].push_back(s);
+            }
+
+            // allocate tablets for each co-location group.
+            // if the base is a new table, allocate new tablets for it.
+            // for the other tables in the group, create a co-located tablet map.
+            for (const auto& [base_id, group_schemas] : table_groups) {
+                if (auto it = new_cfms_map.find(base_id); it != new_cfms_map.end()) {
+                    const auto& s = *it->second;
+                    lblogger.debug("Creating tablets for {}.{} id={}", s.ks_name(), s.cf_name(), s.id());
+                    auto base_map = allocate_tablets_for_new_base_table(tablet_rs, s);
+                    muts.emplace_back(tablet_map_to_mutation(std::move(base_map), s.id(), s.ks_name(), s.cf_name(), ts, _db.features()).get());
+                }
+
+                for (auto sp : group_schemas) {
+                    const auto& s = *sp;
+                    if (s.id() != base_id) {
+                        lblogger.debug("Creating tablets for {}.{} id={} with base={}", s.ks_name(), s.cf_name(), s.id(), base_id);
+                        muts.emplace_back(colocated_tablet_map_to_mutation(s.id(), s.ks_name(), s.cf_name(), base_id, ts));
+                    }
+                }
             }
         }
+    }
+
+    void on_before_create_column_families(const keyspace_metadata& ksm, const std::vector<schema_ptr>& cfms, std::vector<mutation>& muts, api::timestamp_type ts) override {
+        allocate_tablets_for_new_tables(ksm, cfms, muts, ts);
+    }
+
+    void on_before_create_column_family(const keyspace_metadata& ksm, const schema& s, std::vector<mutation>& muts, api::timestamp_type ts) override {
+        allocate_tablets_for_new_tables(ksm, {s.shared_from_this()}, muts, ts);
     }
 
     void on_before_drop_column_family(const schema& s, std::vector<mutation>& muts, api::timestamp_type ts) override {