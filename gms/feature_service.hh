06e4d3982dbe39ef530296101f62e227455bb991
Aleksandra Martyniuk <aleksandra.martyniuk@scylladb.com>
2026-04-17 15:15:11 +0200

    gms: introduce AUTO_REPLICATION_FACTOR cluster feature

--- gms/feature_service.hh
+++ gms/feature_service.hh
@@ -182,6 +182,7 @@ public:
     gms::feature writetime_ttl_individual_element { *this, "WRITETIME_TTL_INDIVIDUAL_ELEMENT"sv };
     gms::feature arbitrary_tablet_boundaries { *this, "ARBITRARY_TABLET_BOUNDARIES"sv };
     gms::feature large_data_virtual_tables { *this, "LARGE_DATA_VIRTUAL_TABLES"sv };
+    gms::feature auto_replication_factor { *this, "AUTO_REPLICATION_FACTOR"sv };
 public:
 
     const std::unordered_map<sstring, std::reference_wrapper<feature>>& registered_features() const;
